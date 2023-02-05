c58c4c7f83cdc62844819ada6c26c50b55a56d89
AYUSH DAYANI <ayushdayani456@gmail.com>
2023-02-04 20:07:38 -0500

    updated js with logic for generating random characters

--- Random-password-generator/index.js
+++ Random-password-generator/index.js
@@ -6,4 +6,51 @@ const nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",]
 
 const specials = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
 
+const charDic = {'cap_char':cap_char, 'low_char': low_char, 'nums': nums, 'specials': specials}
 
+let sliderEl = document.getElementById("myRange")
+let len = sliderEl.value
+let specFlag = false
+let p1 = ""
+let p2 = ""
+
+function spec(){
+    if (specFlag){
+        specFlag = false
+    } else{
+        specFlag = true
+    }
+}
+
+sliderEl.oninput = function() {
+  len = sliderEl.value
+//   console.log(length)
+}
+
+function randomNum(end){
+    return Math.floor(Math.random()*end)
+}
+
+function randomChar(specFlag){
+    typeCh = ['cap_char', 'low_char', 'nums', 'specials']
+    if (specFlag){
+        type = randomNum(4)
+        
+    } else{
+        type = randomNum(3)
+    }
+    charNum = randomNum(charDic[typeCh[type]].length)
+    return charDic[typeCh[type]][charNum]
+}
+
+function generate(){
+    p1 = ""
+    p2 = ""
+    for (i=0; i<len; i++){
+        p1+=randomChar(specFlag)
+    }
+    for (i=0; i<len; i++){
+        p2+=randomChar(specFlag)
+    }
+    console.log(p1+ "  " +p2)
+}
