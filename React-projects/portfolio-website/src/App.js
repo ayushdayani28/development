c89721abd6bfb91f3ac9873513a19e2b4b986e0f
AYUSH DAYANI <ayushdayani456@gmail.com>
2023-04-09 18:37:53 -0400

    secured all links

--- React-projects/portfolio-website/src/App.js
+++ React-projects/portfolio-website/src/App.js
@@ -3,21 +3,6 @@ import React from 'react'
 import Main from './components/Main'
 
 function App() {
-  // const [darkMode, setDarkMode] = React.useState(true)
-    
-  //   function toggleDarkMode() {
-  //       setDarkMode(prevMode => !prevMode)
-  //   }
-  // React.useEffect(() => {
-  //   window.addEventListener("scroll", handleScroll())
-  //   return () => {
-  //       window.removeEventListener("scroll", handleScroll())
-  //   }
-  // }, [])
-  // const handleScroll=()=>{
-  //   console.log('uf')
-  // }
-
   return (
     
     <Main />
--- React-projects/portfolio-website/src/components/Certificates.js
+++ React-projects/portfolio-website/src/components/Certificates.js
@@ -72,7 +72,7 @@ const handlePrevClick = (len) => {
             </li>
           </ul>
           <div className="slider--prev" onClick={() => handlePrevClick(len)}>
-            <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+            <svg version="1.1" id="Layer_1" xmlns="https://www.w3.org/2000/svg" xmlnsXlink="https://www.w3.org/1999/xlink" x="0px" y="0px"
               viewBox="0 0 150 118" style={{enableBackground:"new 0 0 150 118"}} xmlSpace="preserve">
               <g transform="translate(0.000000,118.000000) scale(0.100000,-0.100000)">
                 <path d="M561,1169C525,1155,10,640,3,612c-3-13,1-36,8-52c8-15,134-145,281-289C527,41,562,10,590,10c22,0,41,9,61,29
@@ -82,7 +82,7 @@ const handlePrevClick = (len) => {
             </svg>
           </div>
           <div className="slider--next" onClick={() => handleNextClick(len)}>
-              <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+              <svg version="1.1" id="Layer_1" xmlns="https://www.w3.org/2000/svg" xmlnsXlink="https://www.w3.org/1999/xlink" x="0px" y="0px"
               viewBox="0 0 150 118" style={{enableBackground:"new 0 0 150 118"}} xmlSpace="preserve">
               <g transform="translate(0.000000,118.000000) scale(0.100000,-0.100000)">
                 <path d="M870,1167c-34-17-55-57-46-90c3-15,81-100,194-211l187-185l-565-1c-431,0-571-3-590-13c-55-28-64-94-18-137c21-20,33-20,
